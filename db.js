313304a4ffd0e98a867d61aa38f1e54743e51abd
mahauni <lucasraoniha@gmail.com>
2025-10-30 18:17:24 -0300

    Starting project

---
+++ db.js
@@ -0,0 +1,4 @@
+export const DB = [
+  { operation: "buy", "unit-cost": 20.0, quantity: 10000 },
+  { operation: "sell", "unit-cost": 10.0, quantity: 5000 },
+];
---
+++ main.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const app = express();
+
+app.listen(1234, () => console.log("running it on port 1234"));
