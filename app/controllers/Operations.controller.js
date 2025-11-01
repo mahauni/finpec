37ffc28d20767a10cbe95007c940f14621c7bc4c
mahauni <lucasraoniha@gmail.com>
2025-10-31 21:19:41 -0300

    Adicionando verificacao de request

--- app/controllers/Operations.controller.js
+++ app/controllers/Operations.controller.js
@@ -1,10 +1,45 @@
 import { createOperationsModel } from "../models/Operations.model.js";
 
 export async function postOperation(req, res, next) {
+  const validated = validateBody(req.body);
+
+  if (validated !== "") {
+    res.status(400).json({ message: validated });
+    return;
+  }
+
   try {
     createOperationsModel(req.body);
     res.status(201).json({ message: "Operações registradas com sucesso" });
   } catch (err) {
     return next(err);
   }
+}
+
+function validateBody(body) {
+  if (!Array.isArray(body)) {
+    return "O request deve ser um array";
+  }
+
+  if (body.length < 1) {
+    return "Deve ter no mínimo uma operação";
+  }
+
+  for (let i = 0; i < body.length; i++) {
+    const { operation, ["unit-cost"]: unitCost, quantity } = body[i];
+
+    if (!(operation === "buy" || operation === "sell")) {
+      return "A operação deve ser buy ou sell";
+    }
+
+    if (typeof unitCost !== "number" || unitCost < 0) {
+      return "O unit cost deve ser um número positivo";
+    }
+
+    if (typeof quantity !== "number" || quantity < 0) {
+      return "A quantity deve ser um número positivo";
+    }
+  }
+
+  return "";
 }
